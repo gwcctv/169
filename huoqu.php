954c03d3d8c087e82b387f050a44014da36292db
gwcctv <101457746+gwcctv@users.noreply.github.com>
2024-05-08 16:36:58 +0800

    Update huoqu.php

--- huoqu.php
+++ huoqu.php
@@ -1,110 +1,46 @@
 <?php
-header("Content-type:application/json;charset=utf-8");
 error_reporting(0);
-$url = "http://cms.testipmedia.xyz/newtv/itv/index2.php";
-$specialKey = "p2p20210118";
-$key = substr(md5($specialKey), 0, 16);
-$iv = substr(md5($specialKey), 16, 16);
+set_time_limit(600);
+//http://127.0.0.1/huya3.php
+echo "huya一起看-电影"."\n";
+$str = json_decode(get_data('https://live.huya.com/liveHttpUI/getTmpLiveList?iTmpId=2067&iPageNo=1&iPageSize=120&iLibId=2213&iGid=2135'),1);
+foreach($str['vList'] as $v){
+   $id = $v['lProfileRoom'];
+   $name = $v['sIntroduction'];
+   $d = get_data('https://m.huya.com/'.$id);
+   preg_match('/"sStreamName":"(.*?)"/',$d,$sStreamName);
+   $m3u8 = "http://43.137.29.240/tx.flv.huya.com/src/".$sStreamName[1].".m3u8";
+   echo $name.",".$m3u8."\n";
+   }
+echo "huya一起看-TVB"."\n";
+$str = json_decode(get_data('https://live.huya.com/liveHttpUI/getTmpLiveList?iTmpId=4061&iPageNo=1&iPageSize=120&iLibId=4149&iGid=2135'),1);
+foreach($str['vList'] as $v){
+   $id = $v['lProfileRoom'];
+   $name = $v['sIntroduction'];
+   $d = get_data('https://m.huya.com/'.$id);
+   preg_match('/"sStreamName":"(.*?)"/',$d,$sStreamName);
+   $m3u8 = "http://43.137.29.240/tx.flv.huya.com/src/".$sStreamName[1].".m3u8";
+   echo $name.",".$m3u8."\n";
+   }
 
-$deviceId = strtoupper(randomString(32));
-$Ip = json_decode(curl_get("http://g3.le.com/r?format=1"), true)['host'];
-$getIp = curl_post($url, "getIP=" . $deviceId);
-
-$body = array(
-    "mac" => "5b8a4c0b",
-    "model" => "HD1910",
-    "androidid" => "775D2672A66CC2BF37676E9999510BA7",
-    "deviceid" => $deviceId,
-    "ip" => $Ip,
-    "app_name" => "Focus TV",
-    "package_name" => "com.newtvbaichuan.iptv",
-    
-
-   "token" => "ADA8bC27b3E03AB57E2E15DA9eb8C9",
-    "province" => ""
-);
-$b = gzcompress(json_encode($body, true));
-$enc2 = openssl_encrypt($b, "AES-128-CBC", $key, OPENSSL_RAW_DATA, $iv);
-$res = curl_post($url, "login=" . base64_encode($enc2));
-
-$str = openssl_decrypt(base64_decode($res), "AES-128-CBC", $key, OPENSSL_RAW_DATA, $iv);
-$json = json_decode(gzuncompress($str), true);
-file_put_contents("orginal.txt", json_encode($json, 64 | 128 | 256));
-
-$cate = $json['data'];
-$fp = fopen("iptv.txt", "w");
-$txt = "";
-foreach ($cate as $ct) {
-    if ($ct == '收藏') continue;
-    $txt = $txt . $ct['category'] . ",#genre#\n";
-    $channel = $ct['channels'];
-    for ($i = 0; $i < count($channel); $i++) {
-        $name = trim(str_replace(".", "", $channel[$i]['channelName']));
-        $url = $channel[$i]['channelUrl'][0];
-        $txt = $txt . $name . "," . $url . "\n";
-    }
-}
-fwrite($fp, $txt);
-fclose($fp);
-echo $txt;
-exit();
-
-
-function randomString($length)
+function get_data($url)
 {
-    $characters = '0123456789abcdef';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[random_int(0, $charactersLength - 1)];
-    }
-    return $randomString;
+$ch=curl_init();
+$User_Agent='Mozilla/5.0 (Linux; U; Android 4.0.4; zh-cn; HTC T528w Build/IMM76D) AppleWebKit/533.1 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.4 TBS/025410  Safari/533.1';
+$timeout=5;
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_ENCODING,'');
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch,CURLOPT_HEADER,0);
+curl_setopt($ch,CURLOPT_TIMEOUT,$timeout);
+curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+curl_setopt($ch,CURLOPT_USERAGENT,$User_Agent);
+curl_setopt($ch,CURLOPT_HTTPHEADER,array('X-FORWARDED-FOR:'.$_SERVER['REMOTE_ADDR'],'CLIENT-IP:'.$_SERVER['REMOTE_ADDR']));
+$chr=curl_exec($ch);
+curl_close($ch);
+return $chr;
 }
-
-function curl_get($url)
-{
-    $header = array(
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36'
-    );
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_HEADER, 0);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 20);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    $data = curl_exec($curl);
-    if (curl_error($curl)) {
-        return "Error: " . curl_error($curl);
-    } else {
-        curl_close($curl);
-        return $data;
-    }
-}
-
-function curl_post($url, $postdata)
-{
-    $header = [
-        "User-Agent: Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/94.0.4606.54Safari/537.36",
-    ];
-
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 20);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    curl_setopt($curl, CURLOPT_POST, 1);
-    if ($postdata != null) curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
-    $data = curl_exec($curl);
-    if (curl_error($curl)) {
-        return "Error:" . curl_error($curl);
-    } else {
-        curl_close($curl);
-        return $data;
-    }
-}
-
 ?>
